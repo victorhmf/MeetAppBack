2007cf49ad2b9d92070904ad0e66aeedcc303f2b
Victor Fernandes <victor.hmfd@gmail.com>
2019-04-10 11:22:26 -0300

    returning user with jwt
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,11 +1,19 @@
 'use strict'
 
+const User = use('App/Models/User')
+
 class SessionController {
   async store ({ request, auth }) {
     const { email, password } = request.all()
     const token = await auth.attempt(email, password)
+    const userQuery = await User.query()
+      .where('email', email)
+      .with('preferences')
+      .fetch()
 
-    return token
+    const [user] = userQuery.toJSON()
+
+    return { ...token, user }
   }
 }
 
